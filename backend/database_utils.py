c2d775c5aacf4e5153df84a781b2246f01d8e0ac
Dama Michael Yohanna <get2dama11@gmail.com>
2023-11-27 10:47:35 +0100

    added encryption to all storage field

--- backend/database_utils.py
+++ backend/database_utils.py
@@ -1,53 +1,68 @@
+import datetime
+
 from backend import database, encryptor
 
 
 class DatabaseUtility:
     def __init__(self):
         self.database_handle = database.DatabaseOps()
-        self.encrypt_handle = encryptor.Encryptor()
+        self.encryptor = encryptor.Encryptor()
 
     def insert_user_login(self, username, password, question, answer):
         statement = """INSERT INTO User ('username', 'password', 'question', 'answer') VALUES (?, ?,?,?)"""
-        values = (username, password, question, answer)
+        values = (self.encryptor.encrypt(username),
+                  self.encryptor.encrypt(password),
+                  self.encryptor.encrypt(question),
+                  self.encryptor.encrypt(answer))
         return self.database_handle.insert_record(statement, values)
 
     def login(self, username, password):
+        username = self.encryptor.encrypt(username)
         sql_statement = f"""SELECT pk, username, password FROM User WHERE username='{username}'"""
         user = self.database_handle.fetch_record(sql_statement).fetchone()
         if not user:
             return 0  # return zero if user not found
         else:  # if user exist, check if password matches
-            if password == user[2]:  # compare the given and saved password
-                return [user[0], user[1]]  # return username and id
+            if self.encryptor.encrypt(password) == user[2]:  # compare the given and saved password
+                return [user[0], self.encryptor.decrypt(user[1])]  # return username and id
             else:
                 return 0  # return zero if not the same
 
     def insert_password(self, sitename, username, password, owner):
-        password = self.encrypt_handle.encrypt(password)
+        sitename = self.encryptor.encrypt(sitename)
+        username = self.encryptor.encrypt(username)
+        password = self.encryptor.encrypt(password)
+        date = datetime.datetime.now()
         sql_statement = f"""
-                INSERT INTO Login ('sitename', 'username', 'password', 'owner') 
-                VALUES (?, ?, ?, ?)
+                INSERT INTO Login ('sitename', 'username', 'password', 'owner', 'date') 
+                VALUES (?, ?, ?, ?, ?)
                 """
-        parameter = (sitename, username, password, owner)
-        print(parameter)
+        parameter = (sitename, username, password, owner, date)
         self.database_handle.insert_record(sql_statement, parameter)
 
     def insert_note(self, title, content, owner):
         """function to prepare the sql statement and call the needed function"""
-        content = self.encrypt_handle.encrypt(content)
+        content = self.encryptor.encrypt(content)
+        title = self.encryptor.encrypt(title)
+        date = datetime.datetime.now()
         sql_statement = """
-                INSERT INTO Notes ('title', 'content', 'owner') 
+                INSERT INTO Notes ('title', 'content', 'owner', 'date') 
                 VALUES (?, ?, ?)
                 """
-        parameter = (title, content, owner)
+        parameter = (title, content, owner, date)
         self.database_handle.insert_record(sql_statement, parameter)
 
     def insert_payment(self, bank_name, pin, account, owner):
+        bank_name = self.encryptor.encrypt(bank_name)
+        pin = self.encryptor.encrypt(pin)
+        account = self.encryptor.encrypt(account)
+        date = datetime.datetime.now()
+
         sql_statement = f"""
-                INSERT INTO Payments ('bank', 'pin', 'number', 'owner') 
+                INSERT INTO Payments ('bank', 'pin', 'number', 'owner', 'date') 
                 VALUES (?, ?, ?, ?)
                 """
-        parameter = (bank_name, pin, account, owner)
+        parameter = (bank_name, pin, account, owner, date)
         self.database_handle.insert_record(sql_statement, parameter)
 
     # --------------------------------------------------------------------------------------
