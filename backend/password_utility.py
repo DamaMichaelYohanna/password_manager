94b1079bd813315d0c4eaf155b7006bb59b46785
= <=>
2023-11-28 22:21:03 +0100

    added password longivity check

--- backend/password_utility.py
+++ backend/password_utility.py
@@ -1,5 +1,8 @@
+import datetime
 import random
 import string
+
+from . import database_utils
 
 
 class PasswordManager:
@@ -26,7 +29,8 @@ class PasswordManager:
         # call the private password generator and return it return value.
         return self.__main_password_generator()
 
-    def password_strength_check(self, password: str):
+    @staticmethod
+    def password_strength_check(password: str):
         lower, upper, char, digit = 0, 0, 0, 0
         capital_alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         small_alphabets = "abcdefghijklmnopqrstuvwxyz"
@@ -86,5 +90,14 @@ class PasswordManager:
                     char == 1 or char > 1 and digit == 1):
                 return 7
 
-
-
+    @staticmethod
+    def check_password_timeline(owner):
+        password_list = []
+        return_values = database_utils.DatabaseUtility().fetch_data("Login", owner).fetchall()
+        for value in return_values:
+            year, month, day = str(value[5][:10]).split("-")
+            save_date = datetime.date(int(year), int(month), int(day))
+            current_date = datetime.datetime.now().date()
+            date = current_date - save_date
+            if date >= datetime.timedelta(30):
+                password_list.append(value[2])
